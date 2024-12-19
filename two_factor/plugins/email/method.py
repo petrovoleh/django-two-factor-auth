cfde24144dd2620648dd24e157b5081848cbcdae
Oleh <81036385+petrovoleh@users.noreply.github.com>
2024-12-19 10:50:54 +0200

    Update method.py

--- two_factor/plugins/email/method.py
+++ two_factor/plugins/email/method.py
@@ -21,7 +21,7 @@ class EmailMethod(MethodBase):
         forms = {}
         if not wizard.request.user.email or len(wizard.request.user.email) < 5:
             forms[self.code] = EmailForm
-        forms['validation'] = DeviceValidationForm(device=self.get_devices(wizard.request.user))
+        forms['validation'] = DeviceValidationForm(device=self.get_device_from_setup_data(request=wizard.request))
         return forms
 
     def get_device_from_setup_data(self, request, setup_data, **kwargs):
